1e205ae19657f4a89568afbc81f42cab20d4187c
Jorge Collazo <jorgecollazo@Jorges-Laptop.lan>
2024-11-03 16:31:44 -0700

    added functions to add, subtract, and multiply

--- gitTesting.js
+++ gitTesting.js
@@ -1,5 +1,26 @@
 /* --A simple calculator to add and subtract numbers-- */
 
+function add(){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 + num2
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
+
+function subtract(){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 - num2
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
+
+function multiply(){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 * num2
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
+
 
 /* -- Will take a list of numbers and give the largest or smallest-- */
 
